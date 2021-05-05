df30eb48e3287dbe4449378595c5a63c9e2503ae
nikita juneja <nikita.juneja289@gmail.com>
2021-05-05 12:33:32 +0530

    Added solution for abcdef problem

---
+++ SPOJ/abcdef_problem.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include<bits/stdc++.h>
+ 
+#define ll long long
+#define MOD 1000000007
+using namespace std;
+ 
+int n,lo,hi;
+long long res=0LL;
+vector<int>s1,s2;
+ 
+void findSextuples(int x[], int n){
+    for (int i=0;i<n;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            for (int k=0;k<n;k++)
+            {
+                s1.push_back(x[i]*x[j]+x[k]);
+            }
+        }
+    }
+    for (int i=0;i<n;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            for (int k=0;k<n;k++)
+            {
+                if (x[k]==0) continue;
+                s2.push_back((x[i]+x[j])*x[k]);
+            }
+        }
+    }
+    sort(s1.begin(),s1.end());
+    sort(s2.begin(),s2.end());
+    for (int i=0;i<s1.size();i++)
+    {
+        lo=lower_bound(s2.begin(),s2.end(),s1[i])-s2.begin();
+        hi=upper_bound(s2.begin(),s2.end(),s1[i])-s2.begin();
+        res+=(hi-lo);
+    }
+    printf("%lld\n",res);
+}
+int main()
+{
+    scanf("%d",&n);
+    int x[n];
+    for (int i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    findSextuples(x,n);
+    
+    
+    return 0;
+} 
